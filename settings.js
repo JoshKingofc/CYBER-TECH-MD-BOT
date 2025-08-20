2512b1c050d62d88bac60eae4d4364b62119cb9c
thegame3878 <panashemakanza3@gmail.com>
2025-08-20 23:34:08 +0200

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: ' CYBER-TECH Bot',
+  author: '‎',
+  botName: " CYBER-TECH BOT",
+  botOwner: ' THE GAME', // Your name
+  ownerNumber: ' 263785553857', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.0.8",
+};
+
+module.exports = settings;

318c9b2f0f6b324b8b23e12bdaf101df976f101d
JoshKingofc <joshuamambozindoga04@gmail.com>
2025-08-27 22:43:56 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const settings = {
   packname: ' CYBER-TECH Bot',
   author: '‎',
-  botName: " CYBER-TECH BOT",
-  botOwner: ' THE GAME', // Your name
+  botName: " ‎☘🆃︎🅴︎🅻︎🅺︎🅸︎🅽︎🅶︎☘ BOT",
+  botOwner: ' ‎🇧‌🇴‌🇾‌🇰‌🇦‌ 🇽‌🇩‌', // Your name
   ownerNumber: ' 263785553857', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
